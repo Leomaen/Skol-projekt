558de7b438bbf6575c852a8af57443f2c34f470b
Leomaen <leowik23@varmdogymnasium.se>
2025-05-06 20:39:21 +0200

    fixar

--- Anura/Assets/Scripts/StatsUI.cs
+++ Anura/Assets/Scripts/StatsUI.cs
@@ -1,12 +1,37 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using System.Collections;
 
 public class StatsUI : MonoBehaviour
 {
     public GameObject[] statsSlots;
 
+    private void Awake()
+    {
+        // Subscribe to the item change event
+        StartCoroutine(SubscribeAfterItemManagerInitialized());
+    }
+
+    private IEnumerator SubscribeAfterItemManagerInitialized()
+    {
+        // Wait until ItemManager is available
+        while (ItemManager.Instance == null)
+        {
+            yield return null;
+        }
+        
+        // Now it's safe to subscribe
+        ItemManager.Instance.OnItemsChanged += OnItemsChanged;
+        Debug.Log("Successfully subscribed to OnItemsChanged event");
+    }
+
     private void Start()
+    {
+        UpdateAllStats();
+    }
+
+    private void OnItemsChanged()
     {
         UpdateAllStats();
     }
@@ -17,8 +42,29 @@ public class StatsUI : MonoBehaviour
         statsSlots[0].GetComponentInChildren<TMP_Text>().text = "Speed: " + romanNumeral;
     }
 
+    private void UpdateDamage() {
+        int damageValue = Mathf.RoundToInt(StatsManager.Instance.damage);
+        string romanNumeral = ConvertToRoman(damageValue);
+        statsSlots[1].GetComponentInChildren<TMP_Text>().text = "Damage: " + romanNumeral;
+    }
+
+    private void UpdateatkSpeed() {
+        int atkSpeedValue = Mathf.RoundToInt(StatsManager.Instance.atkSpeed);
+        string romanNumeral = ConvertToRoman(atkSpeedValue);
+        statsSlots[2].GetComponentInChildren<TMP_Text>().text = "ATKSpeed: " + romanNumeral;
+    }
+
+    private void UpdateBulletSpeed() {
+        int bulletSpeedValue = Mathf.RoundToInt(StatsManager.Instance.bulletSpeed);
+        string romanNumeral = ConvertToRoman(bulletSpeedValue);
+        statsSlots[3].GetComponentInChildren<TMP_Text>().text = "BulletSpeed: " + romanNumeral;
+    }
+
     public void UpdateAllStats() {
         UpdateSpeed();
+        UpdateDamage();
+        UpdateatkSpeed();
+        UpdateBulletSpeed();
     }
 
     private string ConvertToRoman(int number)
