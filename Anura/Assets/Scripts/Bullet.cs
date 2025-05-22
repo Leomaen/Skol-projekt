2fd14b1af583228dd053081c1366d54c367f6b86
Leomaen <leowik23@varmdogymnasium.se>
2025-05-22 21:12:01 +0200

    Revert "asd"
    This reverts commit 2b4e9ee8793e7b5216c41ceb1cb82c59c328bc16.

---
+++ Anura/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameState gameState;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.linearVelocity = transform.right * gameState.stats.bulletSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            animator.SetTrigger("hitWall");
+            Destroy(gameObject, 0.5f);
+            rb.linearVelocity = transform.right * 0;
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Check for regular enemy
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.takeDamage();
+                Destroy(gameObject);
+                return;
+            }
+
+            // Check for boss enemy
+            BossEnemy bossEnemy = collision.GetComponent<BossEnemy>();
+            if (bossEnemy != null)
+            {
+                bossEnemy.TakeDamage(gameState.stats.damage);
+                Destroy(gameObject);
+                return;
+            }
+        }
+    }
+}
